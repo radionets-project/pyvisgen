6a9f322f5613c792454c620323dd0e4a42a91f97
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-02-10 11:51:40 +0100

    black

--- vipy/simulation/uv_plots.py
+++ vipy/simulation/uv_plots.py
@@ -2,12 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def plot_baselines(baselines):
-    #plots uv-plane using array of baselines
+    # plots uv-plane using array of baselines
     weight = np.array([b.valid for b in baselines])
     u = np.array([b.u for b in baselines])
     v = np.array([b.v for b in baselines])
     u = u[weight]
     v = v[weight]
-    plt.plot(np.append(u,-u),np.append(v,-v),'x')
+    plt.plot(np.append(u, -u), np.append(v, -v), "x")
