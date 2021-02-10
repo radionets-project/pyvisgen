6e1a1c914ce4ec24004579cc21306bd05f5ac510
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-02-10 12:08:48 +0100

    fix name

--- vipy/simulation/uv_plots.py
+++ vipy/simulation/uv_plots.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def plot_baselines(baseline):
+def plot_baselines(baselines):
     """Takes simulated baselines and creates uv coverage plot.
 
     Parameters
@@ -10,9 +10,9 @@ def plot_baselines(baseline):
     baseline : dataclass object
         baseline object containing all baselines between individual telescopes
     """
-    weight = np.array([b.valid for b in baseline])
-    u = np.array([b.u for b in baseline])
-    v = np.array([b.v for b in baseline])
+    weight = np.array([b.valid for b in baselines])
+    u = np.array([b.u for b in baselines])
+    v = np.array([b.v for b in baselines])
     u = u[weight]
     v = v[weight]
     plt.plot(np.append(u, -u), np.append(v, -v), "x")
