bee0438870b7768744da4c57f1068cfe1fbea40b
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-02-10 11:51:34 +0100

    black, add docstring

--- vipy/simulation/layouts/layouts.py
+++ vipy/simulation/layouts/layouts.py
@@ -1,10 +1,10 @@
-import numpy as np
 from pathlib import Path
 import pandas as pd
 from dataclasses import dataclass
 
 
 file_dir = Path(__file__).parent.resolve()
+
 
 @dataclass
 class Station:
@@ -17,11 +17,35 @@ class Station:
     el_high: float
     sefd: int
     altitude: float
-    
+
 
 def array(array_name):
-    f = array_name + '.txt'
-    array = pd.read_csv(file_dir / f, sep=' ')
-    stations = [Station(row['station_name'], row['X'], row['Y'], row['Z'], row['dish_dia'], row['el_low'], row['el_high'], row['SEFD'], row['altitude']) for index, row in array.iterrows()]
-    return stations
+    """Reads telescope layout txt file and converts it into a dataclass.
 
+    Parameters
+    ----------
+    array_name : str
+        Name of telescope array
+
+    Returns
+    -------
+    stations : dataclass
+        Station infos combinde in dataclass
+    """
+    f = array_name + ".txt"
+    array = pd.read_csv(file_dir / f, sep=" ")
+    stations = [
+        Station(
+            row["station_name"],
+            row["X"],
+            row["Y"],
+            row["Z"],
+            row["dish_dia"],
+            row["el_low"],
+            row["el_high"],
+            row["SEFD"],
+            row["altitude"],
+        )
+        for index, row in array.iterrows()
+    ]
+    return stations
