d5defad5284f77bba72c0a05601c41b6e9d4c712
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-02-15 12:15:14 +0100

    test get_array_layout

--- tests/test_layouts.py
+++ tests/test_layouts.py
@@ -2,3 +2,14 @@ def test_get_array_layout():
     from vipy.layouts.layouts import get_array_layout
 
     layout = get_array_layout("eht")
+
+    assert len(layout) == 8
+    assert type(layout[0].name) == str
+    assert type(layout[0].x) == float
+    assert type(layout[0].y) == float
+    assert type(layout[0].z) == float
+    assert type(layout[0].diam) == float
+    assert type(layout[0].el_low) == int
+    assert type(layout[0].el_high) == int
+    assert type(layout[0].sefd) == int
+    assert type(layout[0].altitude) == int
