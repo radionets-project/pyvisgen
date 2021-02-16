09b5639da1e5c98300214e731b62da841e11d95b
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-02-16 14:21:40 +0100

    enhance dataclass

--- vipy/layouts/layouts.py
+++ vipy/layouts/layouts.py
@@ -1,9 +1,40 @@
 from pathlib import Path
 import pandas as pd
 from dataclasses import dataclass
+import numpy as np
 
 
 file_dir = Path(__file__).parent.resolve()
+
+
+@dataclass
+class Stations:
+    name: [str]
+    x: [float]
+    y: [float]
+    z: [float]
+    diam: [float]
+    el_low: [float]
+    el_high: [float]
+    sefd: [int]
+    altitude: [float]
+
+    def __getitem__(self, i):
+        station = Station(
+            self.name[i],
+            self.x[i],
+            self.y[i],
+            self.z[i],
+            self.diam[i],
+            self.el_low[i],
+            self.el_high[i],
+            self.sefd[i],
+            self.altitude[i],
+        )
+        return station
+
+    def get_station(self, name):
+        return self[np.where(self.name == name)[0][0]]
 
 
 @dataclass
@@ -36,18 +67,15 @@ def get_array_layout(array_name):
     """
     f = array_name + ".txt"
     array = pd.read_csv(file_dir / f, sep=" ")
-    stations = [
-        Station(
-            row["station_name"],
-            row["X"],
-            row["Y"],
-            row["Z"],
-            row["dish_dia"],
-            row["el_low"],
-            row["el_high"],
-            row["SEFD"],
-            row["altitude"],
-        )
-        for index, row in array.iterrows()
-    ]
+    stations = Stations(
+        array["station_name"].values,
+        array["X"].values,
+        array["Y"].values,
+        array["Z"].values,
+        array["dish_dia"].values,
+        array["el_low"].values,
+        array["el_high"].values,
+        array["SEFD"].values,
+        array["altitude"].values,
+    )
     return stations
