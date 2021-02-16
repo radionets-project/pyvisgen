5d7f38efc8a8f364e5dea207cb0057b2f072c435
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-02-16 14:20:59 +0100

    add baseline helper functions

--- vipy/simulation/utils.py
+++ vipy/simulation/utils.py
@@ -1,4 +1,7 @@
 import toml
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+import numpy as np
 
 
 def read_config(conf):
@@ -17,8 +20,11 @@ def read_config(conf):
     config = toml.load(conf)
     sim_conf = {}
 
-    sim_conf["fov_center_ra"] = config["sampling_options"]["fov_center_ra"]
-    sim_conf["fov_center_dec"] = config["sampling_options"]["fov_center_dec"]
+    sim_conf["src_coord"] = SkyCoord(
+        ra=config["sampling_options"]["fov_center_ra"],
+        dec=config["sampling_options"]["fov_center_dec"],
+        unit=(u.hourangle, u.deg),
+    )
     sim_conf["fov_size"] = config["sampling_options"]["fov_size"]
     sim_conf["corr_int_time"] = config["sampling_options"]["corr_int_time"]
     sim_conf["scan_start"] = config["sampling_options"]["scan_start"]
@@ -27,3 +33,33 @@ def read_config(conf):
     sim_conf["channel"] = config["sampling_options"]["channel"]
     sim_conf["interval_length"] = config["sampling_options"]["interval_length"]
     return sim_conf
+
+
+def single_occurance(array):
+    vals, index = np.unique(np.abs(array), return_index=True)
+    return index
+
+
+def get_pairs(array_layout):
+    delta_x = (
+        np.array(
+            [val - array_layout.x[val - array_layout.x != 0] for val in array_layout.x]
+        )
+        .ravel()
+        .reshape(-1, 1)
+    )
+    delta_y = (
+        np.array(
+            [val - array_layout.y[val - array_layout.y != 0] for val in array_layout.y]
+        )
+        .ravel()
+        .reshape(-1, 1)
+    )
+    delta_z = (
+        np.array(
+            [val - array_layout.z[val - array_layout.z != 0] for val in array_layout.z]
+        )
+        .ravel()
+        .reshape(-1, 1)
+    )
+    return delta_x, delta_y, delta_z
