4a32e6388130e3774104c9219a97b7d97e87ce1b
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-02-23 12:02:03 +0100

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="vipy",
-    version="0.0.2",
+    version="0.0.3",
     description="Simulate radio interferometer observations and visibility generation.",
     url="https://github.com/radionets-project/vipy",
     author="Kevin Schmidt, Felix Geyer, Stefan Fröse",
