a8c2200a50e1189735803a1a1822a328d5ce8de5
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2021-12-08 12:15:03 +0100

    read data set config

---
+++ pyvisgen/utils/config.py
@@ -0,0 +1,74 @@
+import toml
+
+
+def read_data_set_conf(conf_toml):
+    """Read toml data set configuration file and convert it into a dictionary.
+
+    Parameters
+    ----------
+    conf : toml file
+        path to config file
+
+    Returns
+    -------
+    sim_conf : dictionary
+        simulation configuration
+    """
+    config = toml.load(conf_toml)
+    conf = {}
+
+    conf["mode"] = config["sampling_options"]["mode"],
+    conf["layout"] = config["sampling_options"]["layout"],
+    conf["img_size"] = config["sampling_options"]["img_size"],
+    conf["fov_center_ra"] = config["sampling_options"]["fov_center_ra"],
+    conf["fov_center_dec"] = config["sampling_options"]["fov_center_dec"],
+    conf["fov_size"] = config["sampling_options"]["fov_size"]
+    conf["corr_int_time"] = config["sampling_options"]["corr_int_time"]
+    conf["scan_start"] = config["sampling_options"]["scan_start"]
+    conf["scan_duration"] = config["sampling_options"]["scan_duration"]
+    conf["scans"] = config["sampling_options"]["scans"]
+    conf["interval_length"] = config["sampling_options"]["interval_length"]
+    conf["base_freq"] = config["sampling_options"]["base_freq"]
+    conf["frequsel"] = config["sampling_options"]["frequsel"]
+    conf["bandwidths"] = config["sampling_options"]["bandwidths"]
+
+    conf["num_bundles"] = config["bundle_options"]["num_bundles"]
+    conf["size_bundles"] = config["bundle_options"]["size_bundles"]
+    conf["num_images"] = conf["num_bundles"] * conf["size_bundles"]
+    conf["in_path"] = config["bundle_options"]["in_path"]
+    conf["out_path"] = config["bundle_options"]["out_path"]
+
+    conf["num_jobs"] = config["cluster_options"]["num_jobs"]
+
+    return conf
+
+
+def read_config(conf):
+    """Read toml simulation configuration file and convert it into a dictionary.
+
+    Parameters
+    ----------
+    conf : toml file
+        path to config file
+
+    Returns
+    -------
+    sim_conf : dictionary
+        simulation configuration
+    """
+    config = toml.load(conf)
+    sim_conf = {}
+
+    sim_conf["src_coord"] = SkyCoord(
+        ra=config["sampling_options"]["fov_center_ra"],
+        dec=config["sampling_options"]["fov_center_dec"],
+        unit=(u.deg, u.deg),
+    )
+    sim_conf["fov_size"] = config["sampling_options"]["fov_size"]
+    sim_conf["corr_int_time"] = config["sampling_options"]["corr_int_time"]
+    sim_conf["scan_start"] = config["sampling_options"]["scan_start"]
+    sim_conf["scan_duration"] = config["sampling_options"]["scan_duration"]
+    sim_conf["scans"] = config["sampling_options"]["scans"]
+    sim_conf["channel"] = config["sampling_options"]["channel"]
+    sim_conf["interval_length"] = config["sampling_options"]["interval_length"]
+    return sim_conf
