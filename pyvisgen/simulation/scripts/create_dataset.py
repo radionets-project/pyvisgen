705809fbd89978239a17a542a4775f80f39fafb2
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2022-03-06 06:59:54 +0100

    add gridding mode

--- pyvisgen/simulation/scripts/create_dataset.py
+++ pyvisgen/simulation/scripts/create_dataset.py
@@ -1,22 +1,27 @@
 import click
 from pyvisgen.simulation.data_set import simulate_data_set
+from pyvisgen.gridding.gridder import create_gridded_data_set
 
 
 @click.command()
 @click.argument("configuration_path", type=click.Path(exists=True, dir_okay=False))
-# @click.option(
-#     "--mode",
-#     type=click.Choice(
-#         [
-#             "simulate",
-#             "overview",
-#         ],
-#         case_sensitive=False,
-#     ),
-#     default="simulate",
-# )
-def main(configuration_path):
-    simulate_data_set(configuration_path)
+@click.option(
+    "--mode",
+    type=click.Choice(
+        [
+            "simulate",
+            "gridding",
+        ],
+        case_sensitive=False,
+    ),
+    default="simulate",
+)
+def main(configuration_path, mode):
+    if mode == "simulate":
+        print("hi")
+        simulate_data_set(configuration_path)
+    if mode == "gridding":
+        create_gridded_data_set(configuration_path)
 
 
 if __name__ == "__main__":
