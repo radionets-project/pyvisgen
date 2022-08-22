17ffe163c166c32d167e6762ded597b4036c1cb5
Felix Geyer <felix.geyer@tu-dortmund.de>
2022-08-22 10:17:06 +0200

    Remove data_handler and beging rework of gridder

--- pyvisgen/utils/data.py
+++ pyvisgen/utils/data.py
@@ -1,68 +1,7 @@
 import h5py
-import torch
 import numpy as np
 import re
 from pathlib import Path
-
-
-class data_handler:
-    def __init__(self, bundle_paths):
-        """
-        Save the bundle paths and the number of bundles in one file.
-        """
-        if bundle_paths == []:
-            raise ValueError('No bundles found! Please check the names of your files.')
-        self.bundles = bundle_paths
-        self.num_img = len(self.open_bundle(self.bundles[0], "x"))
-
-    def __call__(self):
-        return print("This is the h5_dataset class.")
-
-    def __len__(self):
-        """
-        Returns the total number of pictures in this dataset
-        """
-        return len(self.bundles) * self.num_img
-
-    def __getitem__(self, i):
-        x = self.open_image("x", i)
-        y = self.open_image("y", i)
-        return x, y
-
-    def open_bundle(self, bundle_path, var):
-        bundle = h5py.File(bundle_path, "r")
-        data = bundle[var]
-        return data
-
-    def open_image(self, var, i):
-        if isinstance(i, int):
-            i = torch.tensor([i])
-        elif isinstance(i, np.ndarray):
-            i = torch.tensor(i)
-        indices, _ = torch.sort(i)
-        bundle = torch.div(indices, self.num_img, rounding_mode="floor")
-        image = indices - bundle * self.num_img
-        bundle_unique = torch.unique(bundle)
-        bundle_paths = [
-            h5py.File(self.bundles[bundle], "r") for bundle in bundle_unique
-        ]
-        bundle_paths_str = list(map(str, bundle_paths))
-        data = torch.tensor(
-            np.array(
-                [
-                    bund[var][img]
-                    for bund, bund_str in zip(bundle_paths, bundle_paths_str)
-                    for img in image[
-                        bundle == bundle_unique[bundle_paths_str.index(bund_str)]
-                    ]
-                ]
-            )
-        )
-
-        if data.shape[0] == 1:
-            data = data.squeeze(0)
-
-        return data.float()
 
 
 def load_bundles(data_path):
