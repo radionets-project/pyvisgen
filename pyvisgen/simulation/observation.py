48c0a5d09747512fb573ffd8406718413c47ced2
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2023-11-09 15:30:29 +0100

    update observation class

--- pyvisgen/simulation/observation.py
+++ pyvisgen/simulation/observation.py
@@ -1,6 +1,13 @@
 from dataclasses import dataclass
 import torch
 from astropy.time import Time
+import numpy as np
+import astropy.units as un
+from astropy.coordinates import EarthLocation, AltAz, Angle, SkyCoord
+from pyvisgen.layouts import layouts
+import astropy.constants as const
+from math import pi
+from pyvisgen.simulation.utils import Array
 
 
 @dataclass
@@ -47,21 +54,21 @@ class Baseline:
 
 class Observation:
     def __init__(
-            self,
-            src_ra,
-            src_dec,
-            start_time,
-            scan_duration,
-            number_scans,
-            integration_time,
-            pointing_interval_length,
-            base_frequency,
-            frequency_bands,
-            bandwiths,
-            fov,
-            image_size,
-            array_layout,
-        ):
+        self,
+        src_ra,
+        src_dec,
+        start_time,
+        scan_duration,
+        number_scans,
+        scan_separation,
+        integration_time,
+        ref_frequency,
+        spectral_windows,
+        bandwiths,
+        fov,
+        image_size,
+        array_layout,
+    ):
         self.ra = torch.tensor(src_ra).float()
         self.dec = torch.tensor(src_dec).float()
 
@@ -69,42 +76,60 @@ class Observation:
         self.scan_duration = scan_duration
         self.num_scans = number_scans
         self.int_time = integration_time
-        self.interval = pointing_interval_length
+        self.scan_separation = scan_separation
         self.times = self.calc_time_steps()
         self.scans = torch.stack(
-            torch.split(torch.arange(len(self.times)), (len(self.times)//self.num_scans)),
+            torch.split(
+                torch.arange(len(self.times)), (len(self.times) // self.num_scans)
+            ),
             dim=0,
-            )
+        )
 
         self.base_freq = torch.tensor(base_frequency)
         self.frequsel = torch.tensor(frequency_bands)
         self.bandwiths = torch.tensor(bandwiths)
         self.IFs = self.base_freq + self.frequsel
-        self.waves_low = torch.from_numpy((const.c / ( self.IFs - self.bandwiths ) * un.second / un.meter).value)
-        self.waves_high = torch.from_numpy((const.c / ( self.IFs + self.bandwiths ) * un.second / un.meter).value)
-        
+        self.waves_low = torch.from_numpy(
+            (const.c / (self.IFs - self.bandwiths) * un.second / un.meter).value
+        )
+        self.waves_high = torch.from_numpy(
+            (const.c / (self.IFs + self.bandwiths) * un.second / un.meter).value
+        )
+
         self.fov = fov
         self.img_size = image_size
         self.pix_size = fov / image_size
-        
+
         self.array = layouts.get_array_layout(array_layout)
-        
+
         self.rd = self.create_rd_grid()
         self.lm = self.create_lm_grid()
-        
+
         self.calc_baselines()
-        self.baselines.num = int(len(self.array.st_num) * (len(self.array.st_num) - 1) / 2)
+        self.baselines.num = int(
+            len(self.array.st_num) * (len(self.array.st_num) - 1) / 2
+        )
         self.calc_valid_baselines()
-        
+
     def calc_baselines(self):
-        self.baselines = Baselines(torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]))
+        self.baselines = Baselines(
+            torch.tensor([]),
+            torch.tensor([]),
+            torch.tensor([]),
+            torch.tensor([]),
+            torch.tensor([]),
+            torch.tensor([]),
+        )
         for scan in self.scans:
             bas = self.get_baselines(self.times[scan])
             self.baselines.add(bas)
 
     def calc_time_steps(self):
         time_lst = [
-            self.start + self.interval * i * un.second + j * self.int_time * un.second
+            self.start
+            + self.scan_separation * i * un.second
+            + i * self.scan_duration * un.second
+            + j * self.int_time * un.second
             for i in range(self.num_scans)
             for j in range(int(self.scan_duration / self.int_time) + 1)
         ]
@@ -113,15 +138,12 @@ class Observation:
         time = Time(time_lst)
         return time
 
-
     def calc_ref_elev(self, time=None):
-        if time == None:
+        if time is None:
             time = self.times
         if time.shape == ():
             time = time[None]
-        src_crd = SkyCoord(
-            ra=src_ra, dec=src_dec, unit=(un.deg, un.deg)
-        )
+        src_crd = SkyCoord(ra=self.ra, dec=self.dec, unit=(un.deg, un.deg))
         # Calculate for all times
         # calculate GHA, Greenwich as reference
         ha_all = Angle(
@@ -143,7 +165,6 @@ class Observation:
         assert len(ha_all.value) == len(el_st_all)
         return torch.tensor(ha_all.deg), torch.tensor(el_st_all.alt.degree)
 
-
     def test_active_telescopes(self):
         _, el_st_0 = self.calc_ref_elev(self.times[0])
         _, el_st_1 = self.calc_ref_elev(self.times[1])
@@ -152,7 +173,6 @@ class Observation:
         active_telescopes_0 = np.sum((el_st_0 >= el_min) & (el_st_0 <= el_max))
         active_telescopes_1 = np.sum((el_st_1 >= el_min) & (el_st_1 <= el_max))
         return min(active_telescopes_0, active_telescopes_1)
-        
 
     def create_rd_grid(self):
         """Calculates RA and Dec values for a given fov around a source position
@@ -163,9 +183,9 @@ class Observation:
             FOV size
         samples : int
             number of pixels
-        src_ra : 
+        src_ra :
             right ascensio of the source in deg
-        src_dec : 
+        src_dec :
             dec of the source in deg
 
         Returns
@@ -178,17 +198,16 @@ class Observation:
 
         # define resolution
         res = fov / self.img_size
-        
+
         ra = torch.deg2rad(self.ra)
         dec = torch.deg2rad(self.dec)
-        r = (torch.arange(self.img_size) - self.img_size / 2)  * res + ra
-        d = -(torch.arange(self.img_size) - self.img_size / 2)  * res + dec
+        r = (torch.arange(self.img_size) - self.img_size / 2) * res + ra
+        d = -(torch.arange(self.img_size) - self.img_size / 2) * res + dec
         _, R = torch.meshgrid((r, r), indexing="ij")
         D, _ = torch.meshgrid((d, d), indexing="ij")
         rd_grid = torch.cat([R[..., None], D[..., None]], dim=2)
         return rd_grid
-    
-        
+
     def create_lm_grid(self):
         """Calculates sine projection for fov
 
@@ -208,13 +227,16 @@ class Observation:
         lm_grid[:, :, 0] = torch.cos(self.rd[:, :, 1]) * torch.sin(
             self.rd[:, :, 0] - torch.deg2rad(self.ra)
         )
-        lm_grid[:, :, 1] = torch.sin(self.rd[:, :, 1]) * torch.cos(torch.deg2rad(self.dec)) - torch.cos(
+        lm_grid[:, :, 1] = torch.sin(self.rd[:, :, 1]) * torch.cos(
             torch.deg2rad(self.dec)
-        ) * torch.sin(np.deg2rad(self.dec)) * torch.cos(self.rd[:, :, 0] - torch.deg2rad(self.ra))
+        ) - torch.cos(torch.deg2rad(self.dec)) * torch.sin(
+            np.deg2rad(self.dec)
+        ) * torch.cos(
+            self.rd[:, :, 0] - torch.deg2rad(self.ra)
+        )
 
         return lm_grid
-    
-    
+
     def get_baselines(self, times):
         """Calculates baselines from source coordinates and time of observation for
         every antenna station in array_layout.
@@ -243,7 +265,14 @@ class Observation:
         st_num_pairs, els_low_pairs, els_high_pairs = ar.calc_ant_pair_vals
 
         # Loop over ha and el_st
-        baselines = Baselines(torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]), torch.tensor([]))
+        baselines = Baselines(
+            torch.tensor([]),
+            torch.tensor([]),
+            torch.tensor([]),
+            torch.tensor([]),
+            torch.tensor([]),
+            torch.tensor([]),
+        )
         for ha, el_st in zip(ha_all, el_st_all):
             u, v, w = self.calc_direction_cosines(ha, el_st, delta_x, delta_y, delta_z)
 
@@ -296,21 +325,23 @@ class Observation:
         return u, v, w
 
     def calc_valid_baselines(self, time=None):
-        if time == None:
+        if time is None:
             time = self.times
         valid = self.baselines.valid.reshape(-1, self.baselines.num)
         print(valid)
         mask = valid[:-1].bool() & valid[1:].bool()
-        
+
         self.u_start = self.baselines.u.reshape(-1, self.baselines.num)[:-1][mask]
         self.u_stop = self.baselines.u.reshape(-1, self.baselines.num)[1:][mask]
         self.v_start = self.baselines.v.reshape(-1, self.baselines.num)[:-1][mask]
         self.v_stop = self.baselines.v.reshape(-1, self.baselines.num)[1:][mask]
         self.w_start = self.baselines.w.reshape(-1, self.baselines.num)[:-1][mask]
         self.w_stop = self.baselines.w.reshape(-1, self.baselines.num)[1:][mask]
-    
+
         self.date = torch.repeat_interleave(
-            torch.from_numpy((time[:-1] + self.int_time * un.second / 2).jd.reshape(-1, 1)),
+            torch.from_numpy(
+                (time[:-1] + self.int_time * un.second / 2).jd.reshape(-1, 1)
+            ),
             self.baselines.num,
             dim=1,
         )[mask]
