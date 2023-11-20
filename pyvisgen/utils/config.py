a13f10c80312b8f09f7e6b78890c970773667787
Felix Geyer <felix.geyer@tu-dortmund.de>
2023-11-20 12:35:06 +0100

    Delete unused function

--- pyvisgen/utils/config.py
+++ pyvisgen/utils/config.py
@@ -1,6 +1,4 @@
-import astropy.units as un
 import toml
-from astropy.coordinates import SkyCoord
 
 
 def read_data_set_conf(conf_toml):
@@ -45,34 +43,3 @@ def read_data_set_conf(conf_toml):
     conf["out_path_fits"] = config["bundle_options"]["out_path_fits"]
     conf["out_path_gridded"] = config["bundle_options"]["out_path_gridded"]
     return conf
-
-
-def read_config(conf):
-    """Read toml simulation configuration file and convert it into a dictionary.
-
-    Parameters
-    ----------
-    conf : toml file
-        path to config file
-
-    Returns
-    -------
-    sim_conf : dictionary
-        simulation configuration
-    """
-    config = toml.load(conf)
-    sim_conf = {}
-
-    sim_conf["src_coord"] = SkyCoord(
-        ra=config["sampling_options"]["fov_center_ra"],
-        dec=config["sampling_options"]["fov_center_dec"],
-        unit=(un.deg, un.deg),
-    )
-    sim_conf["fov_size"] = config["sampling_options"]["fov_size"]
-    sim_conf["corr_int_time"] = config["sampling_options"]["corr_int_time"]
-    sim_conf["scan_start"] = config["sampling_options"]["scan_start"]
-    sim_conf["scan_duration"] = config["sampling_options"]["scan_duration"]
-    sim_conf["scans"] = config["sampling_options"]["scans"]
-    sim_conf["channel"] = config["sampling_options"]["channel"]
-    sim_conf["interval_length"] = config["sampling_options"]["interval_length"]
-    return sim_conf
