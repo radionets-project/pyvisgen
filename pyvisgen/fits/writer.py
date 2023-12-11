Fix calculation of K

pyvisgen/fits/writer.py
@@ -185,7 +185,7 @@ def create_frequency_hdu(conf):
     col1 = fits.Column(name="FRQSEL", format="1J", unit=" ", array=FRQSEL)
 
     IF_FREQ = np.array(
-        [np.array(conf["spectral_windows"]) - np.array(conf["ref_frequency"])],
+        [np.array(conf["spectral_windows"])],
         dtype=">f8",
     )  # start with 0, add ch_with per IF
     col2 = fits.Column(

pyvisgen/simulation/scan.py
@@ -105,7 +105,7 @@ class Integrate(nn.Module):
 
 
 class RIME_uncorrupted(nn.Module):
-    def __init__(self, bas, obs, spw, device, grad):
+    def __init__(self, bas, obs, spw_low, spw_high, device, grad):
         """Calculates uncorrupted visibility
 
         Parameters
@@ -125,13 +125,15 @@ class RIME_uncorrupted(nn.Module):
         super().__init__()
         self.bas = bas
         self.obs = obs
-        self.fourier = FourierKernel(bas, obs, spw, device)
+        self.fourier_low = FourierKernel(bas, obs, spw_low, device)
+        self.fourier_high = FourierKernel(bas, obs, spw_high, device)
         self.integrate = Integrate()
 
     def forward(self, img):
         with torch.no_grad():
-            K = self.fourier(img)
-            vis = self.integrate(K, K)
+            K1 = self.fourier_low(img)
+            K2 = self.fourier_high(img)
+            vis = self.integrate(K1, K2)
             return vis
 
 

pyvisgen/simulation/visibility.py
@@ -72,6 +72,7 @@ class Vis:
 
 def vis_loop(rc, SI, num_threads=10, noisy=True):
     torch.set_num_threads(num_threads)
+    IFs = get_IFs(rc)
 
     obs = Observation(
         src_ra=rc["fov_center_ra"],
@@ -82,7 +83,7 @@ def vis_loop(rc, SI, num_threads=10, noisy=True):
         scan_separation=rc["scan_separation"],
         integration_time=rc["corr_int_time"],
         ref_frequency=rc["ref_frequency"],
-        spectral_windows=rc["spectral_windows"],
+        spectral_windows=IFs,
         bandwidths=rc["bandwidths"],
         fov=rc["fov_size"],
         image_size=rc["img_size"],
@@ -119,17 +120,23 @@ def vis_loop(rc, SI, num_threads=10, noisy=True):
             if len(bas_t.valid_t[bas_t.valid_t != 0]) == 0:
                 continue
 
+            spws = [
+                calc_windows(torch.tensor(IF), torch.tensor(bandwidth))
+                for IF, bandwidth in zip(IFs, rc["bandwidths"])
+            ]
+
             int_values = torch.cat(
                 [
                     calc_vis(
                         bas_t,
                         obs,
-                        torch.tensor(spw),
+                        spw[0],
+                        spw[1],
                         SI,
                         corrupted=rc["corrupted"],
                         device=rc["device"],
                     )[None]
-                    for spw in rc["spectral_windows"]
+                    for spw in spws
                 ]
             )
 
@@ -160,12 +167,14 @@ def vis_loop(rc, SI, num_threads=10, noisy=True):
     return visibilities
 
 
-def calc_vis(bas, obs, spw, SI, corrupted=False, device="cpu"):
+def calc_vis(bas, obs, spw_low, spw_high, SI, corrupted=False, device="cpu"):
     if corrupted:
         print("Currently not supported!")
         return -1
     else:
-        rime = scan.RIME_uncorrupted(bas, obs, spw, device=device, grad=False)
+        rime = scan.RIME_uncorrupted(
+            bas, obs, spw_low, spw_high, device=device, grad=False
+        )
         int_values = rime(SI.permute(dims=(1, 2, 0)).to(torch.device(device)))
     return int_values
 
@@ -193,3 +202,12 @@ def generate_noise(shape, rc):
     noise = noise + 1.0j * torch.normal(mean=0, std=std, size=shape)
 
     return noise
+
+
+def get_IFs(rc):
+    IFs = [rc["ref_frequency"] + float(freq) for freq in rc["spectral_windows"]]
+    return IFs
+
+
+def calc_windows(spw, bandwidth):
+    return spw - bandwidth * 0.5, spw + bandwidth * 0.5