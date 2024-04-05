050cfaecd1f1b1fda3343911140876361ba25462
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2024-04-05 15:36:20 +0200

    smaller split chunks

--- pyvisgen/simulation/visibility.py
+++ pyvisgen/simulation/visibility.py
@@ -86,7 +86,7 @@ def vis_loop(obs, SI, num_threads=10, noisy=True, mode="full"):
     else:
         raise ValueError("Unsupported mode!")
 
-    for p in torch.arange(bas[:].shape[1]).split(500):
+    for p in torch.arange(bas[:].shape[1]).split(250):
         bas_p = bas[:][:, p]
 
         int_values = torch.cat(
