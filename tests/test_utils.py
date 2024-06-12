8775e39305ee857a00705174ca53736db432a288
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-06-12 16:10:02 +0200

    Fix tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -72,13 +72,9 @@ def test_Array():
 
     array_layout = get_array_layout("vlba")
     ar = Array(array_layout)
-    delta_x, delta_y, delta_z, indices = ar.calc_relative_pos
-
-    mask = ar.get_baseline_mask
+    delta_x, delta_y, delta_z = ar.calc_relative_pos
 
     st_num_pairs, els_low_pairs, els_high_pairs = ar.calc_ant_pair_vals
 
     assert delta_x.shape == delta_y.shape == delta_z.shape == (45, 1)
-    assert indices.shape == (45,)
-    assert len(mask) == 10
     assert st_num_pairs.shape == els_low_pairs.shape == els_high_pairs.shape == (45, 2)
