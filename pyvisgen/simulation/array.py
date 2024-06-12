c10c1dc2c2501417f03730d4db8631ed81242b48
Anno Knierim <anno.knierim@tu-dortmund.de>
2024-06-12 15:33:17 +0200

    Rewrite antenna pair calculations

--- pyvisgen/simulation/array.py
+++ pyvisgen/simulation/array.py
@@ -10,111 +10,28 @@ class Array:
     def calc_relative_pos(self):
         # from geocentric coordinates to relative coordinates inside array
         delta_x, delta_y, delta_z = self.get_pairs()
-        self.indices = self.single_occurance(delta_x)
-        delta_x = delta_x[self.indices]
-        delta_y = delta_y[self.indices]
-        delta_z = delta_z[self.indices]
-        return delta_x, delta_y, delta_z, self.indices
-
-    def get_pairs(self):
-        delta_x = (
-            torch.stack(
-                [
-                    val
-                    - self.array_layout.x[
-                        ~(torch.arange(len(self.array_layout.x)) == i)
-                    ]
-                    for i, val in enumerate(self.array_layout.x)
-                ]
-            )
-            .ravel()
-            .reshape(-1, 1)
-        )
-        delta_y = (
-            torch.stack(
-                [
-                    val
-                    - self.array_layout.y[
-                        ~(torch.arange(len(self.array_layout.y)) == i)
-                    ]
-                    for i, val in enumerate(self.array_layout.y)
-                ]
-            )
-            .ravel()
-            .reshape(-1, 1)
-        )
-        delta_z = (
-            torch.stack(
-                [
-                    val
-                    - self.array_layout.z[
-                        ~(torch.arange(len(self.array_layout.z)) == i)
-                    ]
-                    for i, val in enumerate(self.array_layout.z)
-                ]
-            )
-            .ravel()
-            .reshape(-1, 1)
-        )
         return delta_x, delta_y, delta_z
 
-    def single_occurance(self, tensor):
-        # only calc one half of visibility because of Fourier symmetry
-        vals, index = self.unique(torch.abs(tensor))
-        return index
-
-    def unique(self, x, dim=0):
-        unique, inverse, counts = torch.unique(
-            x, dim=dim, sorted=True, return_inverse=True, return_counts=True
-        )
-        inv_sorted = inverse.argsort(stable=True)
-        tot_counts = torch.cat((counts.new_zeros(1), counts.cumsum(dim=0)))[:-1]
-        index = inv_sorted[tot_counts]
-        index = index
-        return unique, index
-
     @lazyproperty
-    def get_baseline_mask(self):
-        # mask baselines between the same telescope
-        self.mask = [
-            i * len(self.array_layout.x) + i for i in range(len(self.array_layout.x))
-        ]
-        return self.mask
+    def get_pairs(self):
+        combs_x = torch.combinations(self.array_layout.x)
+        delta_x = (combs_x[:, 0] - combs_x[:, 1]).reshape(-1, 1)
 
-    def delete(self, arr: torch.Tensor, ind: int, dim: int) -> torch.Tensor:
-        skip = [i for i in range(arr.size(dim)) if i != ind]
-        indices = [slice(None) if i != dim else skip for i in range(arr.ndim)]
-        return arr.__getitem__(indices)
+        combs_y = torch.combinations(self.array_layout.y)
+        delta_y = (combs_y[:, 0] - combs_y[:, 1]).reshape(-1, 1)
+
+        combs_z = torch.combinations(self.array_layout.z)
+        delta_z = (combs_z[:, 0] - combs_z[:, 1]).reshape(-1, 1)
+
+        return delta_x, delta_y, delta_z
 
     @lazyproperty
     def calc_ant_pair_vals(self):
-        st_num_pairs = self.delete(
-            arr=torch.stack(
-                torch.meshgrid(self.array_layout.st_num, self.array_layout.st_num)
-            )
-            .swapaxes(0, 2)
-            .reshape(-1, 2),
-            ind=self.mask,
-            dim=0,
-        )[self.indices]
+        """Calculates station number, low elevation, and high
+        elevation pairs.
+        """
+        st_num_pairs = torch.combinations(self.array_layout.st_num)
+        els_low_pairs = torch.combinations(self.array_layout.el_low)
+        els_high_pairs = torch.combinations(self.array_layout.el_high)
 
-        els_low_pairs = self.delete(
-            arr=torch.stack(
-                torch.meshgrid(self.array_layout.el_low, self.array_layout.el_low)
-            )
-            .swapaxes(0, 2)
-            .reshape(-1, 2),
-            ind=self.mask,
-            dim=0,
-        )[self.indices]
-
-        els_high_pairs = self.delete(
-            arr=torch.stack(
-                torch.meshgrid(self.array_layout.el_high, self.array_layout.el_high)
-            )
-            .swapaxes(0, 2)
-            .reshape(-1, 2),
-            ind=self.mask,
-            dim=0,
-        )[self.indices]
         return st_num_pairs, els_low_pairs, els_high_pairs
