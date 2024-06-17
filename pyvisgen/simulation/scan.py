72926414de3ca2ceb7d0377b1f8028e0dde079a3
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2024-06-17 11:19:27 +0200

    transform rime into nn module

--- pyvisgen/simulation/scan.py
+++ pyvisgen/simulation/scan.py
@@ -2,47 +2,71 @@ from math import pi
 
 import torch
 from astropy.constants import c
+from torch import nn
 from torch.special import bessel_j1
 
 
+class RIME(nn.Module):
+    def __init__(
+        self,
+        bas,
+        lm,
+        rd,
+        ra,
+        dec,
+        ant_diam,
+        spw_low,
+        spw_high,
+        corrupted=False,
+    ):
+        """Calculates visibilities using RIME
+
+        Parameters
+        ----------
+        img: torch.tensor
+            sky distribution
+        bas : dataclass object
+            baselines dataclass
+        lm : 2d array
+            lm grid for FOV
+        spw_low : float
+            lower wavelength
+        spw_high : float
+            higher wavelength
+
+        Returns
+        -------
+        2d tensor
+            Returns visibility for every baseline
+        """
+        super().__init__()
+        self.fourier_kernel = fourier_kernel(bas, lm, spw_low, spw_high)
+
+        self.corrupted = corrupted
+        if self.corrupted:
+            self.beam = calc_beam(rd, ra, dec, ant_diam, spw_low, spw_high)
+
+    def forward(self, img):
+        with torch.no_grad():
+            K1, K2 = self.fourier_kernel
+            X1, X2 = img * K1, img * K2
+            del K1, K2
+            if self.corrupted:
+                E1, E2 = self.beam
+                X1 = E1[..., None] * X1 * E1[..., None]
+                del E1
+                X2 = E2[..., None] * X2 * E2[..., None]
+                del E2
+            vis = integrate(X1, X2)
+        return vis
+
+
 @torch.compile
-def rime(img, bas, lm, rd, ra, dec, ant_diam, spw_low, spw_high, corrupted=False):
-    """Calculates visibilities using RIME
-
-    Parameters
-    ----------
-    img: torch.tensor
-        sky distribution
-    bas : dataclass object
-        baselines dataclass
-    lm : 2d array
-        lm grid for FOV
-    spw_low : float
-        lower wavelength
-    spw_high : float
-        higher wavelength
-
-    Returns
-    -------
-    2d tensor
-        Returns visibility for every baseline
-    """
-    with torch.no_grad():
-        X1, X2 = calc_fourier(img, bas, lm, spw_low, spw_high)
-        if corrupted:
-            X1, X2 = calc_beam(X1, X2, rd, ra, dec, ant_diam, spw_low, spw_high)
-        vis = integrate(X1, X2)
-    return vis
-
-
-@torch.compile
-def calc_fourier(img, bas, lm, spw_low, spw_high):
+def fourier_kernel(bas, lm, spw_low, spw_high):
     """Calculates Fouriertransformation Kernel for every baseline and pixel in lm grid.
 
     Parameters
     ----------
-    img: torch.tensor
-        sky distribution
     bas : dataclass object
         baseline information
     lm : 2d array
@@ -84,11 +108,11 @@ def calc_fourier(img, bas, lm, spw_low, spw_high):
         * (ul / c.value * spw_high + vm / c.value * spw_high + wn / c.value * spw_high)
     )[..., None, None]
     del ul, vm, wn
-    return img * K1, img * K2
+    return K1, K2
 
 
 @torch.compile
-def calc_beam(X1, X2, rd, ra, dec, ant_diam, spw_low, spw_high):
+def calc_beam(rd, ra, dec, ant_diam, spw_low, spw_high):
     diameters = ant_diam.to(rd.device)
     theta = angularDistance(rd, ra, dec)
     tds = diameters * theta[..., None]
@@ -97,13 +121,7 @@ def calc_beam(X1, X2, rd, ra, dec, ant_diam, spw_low, spw_high):
     E2 = jinc(2 * pi / c.value * spw_high * tds)
 
     assert E1.shape == E2.shape
-
-    EXE1 = E1[..., None] * X1 * E1[..., None]
-    del E1, X1
-
-    EXE2 = E2[..., None] * X2 * E2[..., None]
-    del E2, X2
-    return EXE1, EXE2
+    return E1, E2
 
 
 @torch.compile
