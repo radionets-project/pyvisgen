Fix lm grid calculation (#32)
* Fix lm grid calculation

* Remove redundant ra in lm_grid

* Fix dtypes and reintroduce ra variable

* Refactor K matrix calculation

* Change dtypes of pointings

* Remove r.a. addition of source position from lm grid calculation

* add changelog

* use c from scipy constants

* add changelog

* pre-commit changes

* Add ra back to calculations

* Final removal of ra and subtraction of ra in scan.py

---------

Co-authored-by: Tom Groß <tom.gross@udo.edu>
Co-authored-by: Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
Co-authored-by: Felix Geyer <felix.geyer@tu-dortmund.de>

pyvisgen/simulation/scan.py
@@ -1,7 +1,7 @@
 from math import pi
 
 import torch
-from astropy.constants import c
+from scipy.constants import c
 from torch.special import bessel_j1
 
 
@@ -71,18 +71,8 @@ def calc_fourier(img, bas, lm, spw_low, spw_high):
     wn = w_cmplt[..., None] * (n - 1)
     del l, m, n, u_cmplt, v_cmplt, w_cmplt
 
-    K1 = torch.exp(
-        -2
-        * pi
-        * 1j
-        * (ul / c.value * spw_low + vm / c.value * spw_low + wn / c.value * spw_low)
-    )[..., None, None]
-    K2 = torch.exp(
-        -2
-        * pi
-        * 1j
-        * (ul / c.value * spw_high + vm / c.value * spw_high + wn / c.value * spw_high)
-    )[..., None, None]
+    K1 = torch.exp(-2 * pi * 1j * (ul + vm + wn) / c * spw_low)[..., None, None]
+    K2 = torch.exp(-2 * pi * 1j * (ul + vm + wn) / c * spw_high)[..., None, None]
     del ul, vm, wn
     return img * K1, img * K2
 
@@ -93,8 +83,8 @@ def calc_beam(X1, X2, rd, ra, dec, ant_diam, spw_low, spw_high):
     theta = angularDistance(rd, ra, dec)
     tds = diameters * theta[..., None]
 
-    E1 = jinc(2 * pi / c.value * spw_low * tds)
-    E2 = jinc(2 * pi / c.value * spw_high * tds)
+    E1 = jinc(2 * pi / c * spw_low * tds)
+    E2 = jinc(2 * pi / c * spw_high * tds)
 
     assert E1.shape == E2.shape
 
@@ -125,7 +115,7 @@ def angularDistance(rd, ra, dec):
         Returns angular Distance for every pixel in rd grid with respect
         to source position
     """
-    r = rd[..., 0] - torch.deg2rad(ra.to(rd.device))
+    r = rd[..., 0]
     d = rd[..., 1] - torch.deg2rad(dec.to(rd.device))
     theta = torch.arcsin(torch.sqrt(r**2 + d**2))
     return theta
@@ -168,15 +158,20 @@ def integrate(X1, X2):
     """
     X_f = torch.stack((X1, X2))
     int_m = torch.sum(X_f, dim=2)
+
     del X_f
+
     # only integrate for 1 sky dimension
     # 2d sky is reshaped to 1d by sensitivity mask
     # int_l = torch.sum(int_m, dim=2)
     # del int_m
     int_f = 0.5 * torch.sum(int_m, dim=0)
     del int_m
+
     X_t = torch.stack(torch.split(int_f, int(int_f.shape[0] / 2), dim=0))
     del int_f
+
     int_t = 0.5 * torch.sum(X_t, dim=0)
     del X_t
+
     return int_t

pyvisgen/simulation/visibility.py
@@ -1,10 +1,9 @@
 from dataclasses import dataclass, fields
 
 import torch
+from tqdm import tqdm
 
 import pyvisgen.simulation.scan as scan
-
-from tqdm import tqdm
 
 
 @dataclass
@@ -39,7 +38,15 @@ class Visibilities:
         ]
 
 
-def vis_loop(obs, SI, num_threads=10, noisy=True, mode="full", batch_size=100, show_progress=False):
+def vis_loop(
+    obs,
+    SI,
+    num_threads=10,
+    noisy=True,
+    mode="full",
+    batch_size=100,
+    show_progress=False,
+):
     torch.set_num_threads(num_threads)
     torch._dynamo.config.suppress_errors = True
 
@@ -96,10 +103,10 @@ def vis_loop(obs, SI, num_threads=10, noisy=True, mode="full", batch_size=100, s
         raise ValueError("Unsupported mode!")
 
     batches = torch.arange(bas[:].shape[1]).split(batch_size)
-    
+
     if show_progress:
         batches = tqdm(batches)
-        
+
     for p in batches:
         bas_p = bas[:][:, p]
 