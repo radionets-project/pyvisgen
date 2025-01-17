Fix feed rotation computation

pyvisgen/simulation/scan.py
@@ -43,11 +43,11 @@ def rime(
     """
     with torch.no_grad():
         X1, X2 = calc_fourier(img, bas, lm, spw_low, spw_high)
-        print(X1.shape)
+
         if corrupted:
             X1, X2 = calc_beam(X1, X2, rd, ra, dec, ant_diam, spw_low, spw_high)
 
-        X1, X2 = calc_feed_rotation(X1, X2, bas.q1, bas.q2, polarisation)
+        X1, X2 = calc_feed_rotation(X1, X2, bas, polarisation)
         vis = integrate(X1, X2)
     return vis
 
@@ -79,7 +79,7 @@ def calc_fourier(img, bas, lm, spw_low, spw_high):
     v_cmplt = torch.cat((bas[3], bas[4]))
     w_cmplt = torch.cat((bas[6], bas[7]))
 
-    l = lm[..., 0]
+    l = lm[..., 0]  # noqa: E741
     m = lm[..., 1]
     n = torch.sqrt(1 - l**2 - m**2)
 
@@ -95,34 +95,34 @@ def calc_fourier(img, bas, lm, spw_low, spw_high):
 
 
 @torch.compile
-def calc_feed_rotation(X1, X2, q1, q2, polarisation):
+def calc_feed_rotation(X1, X2, bas, polarisation):
     """ """
-    P1 = torch.ones_like(X1)
-    P2 = torch.ones_like(X2)
+    q1 = torch.cat((bas[11], bas[12]))[..., None]
+    q2 = torch.cat((bas[14], bas[15]))[..., None]
 
     if polarisation == "linear":
-        P1[..., 0, 0] = torch.cos(q1)
-        P1[..., 0, 1] = torch.sin(q1)
-        P1[..., 1, 0] = -torch.sin(q1)
-        P1[..., 1, 1] = torch.cos(q1)
+        X1[..., 0, 0] *= torch.cos(q1)
+        X1[..., 0, 1] *= torch.sin(q1)
+        X1[..., 1, 0] *= -torch.sin(q1)
+        X1[..., 1, 1] *= torch.cos(q1)
 
-        P2[..., 0, 0] = torch.cos(q2)
-        P2[..., 0, 1] = torch.sin(q2)
-        P2[..., 1, 0] = -torch.sin(q2)
-        P2[..., 1, 1] = torch.cos(q2)
+        X2[..., 0, 0] *= torch.cos(q2)
+        X2[..., 0, 1] *= torch.sin(q2)
+        X2[..., 1, 0] *= -torch.sin(q2)
+        X2[..., 1, 1] *= torch.cos(q2)
 
     if polarisation == "circular":
-        P1[..., 0, 0] = torch.exp(1j * q1)
-        P1[..., 0, 1] = 0
-        P1[..., 1, 0] = 0
-        P1[..., 1, 1] = torch.exp(-1j * q1)
+        X1[..., 0, 0] *= torch.exp(1j * q1)
+        X1[..., 0, 1] *= 0
+        X1[..., 1, 0] *= 0
+        X1[..., 1, 1] *= torch.exp(-1j * q1)
 
-        P2[..., 0, 0] = torch.exp(1j * q2)
-        P2[..., 0, 1] = 0
-        P2[..., 1, 0] = 0
-        P2[..., 1, 1] = torch.exp(-1j * q2)
+        X2[..., 0, 0] *= torch.exp(1j * q2)
+        X2[..., 0, 1] *= 0
+        X2[..., 1, 0] *= 0
+        X2[..., 1, 1] *= torch.exp(-1j * q2)
 
-    return img * P1, img * P2
+    return X1, X2
 
 
 @torch.compile