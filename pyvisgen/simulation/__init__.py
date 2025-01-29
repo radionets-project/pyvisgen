78022889295a9e2d03612ba640b38510d974025e
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-01-29 16:59:10 +0100

    Update pyvisgen.simulation.__init__

--- pyvisgen/simulation/__init__.py
+++ pyvisgen/simulation/__init__.py
@@ -1,14 +1,5 @@
 from .array import Array
-from .data_set import (
-    calc_ref_elev,
-    calc_time_steps,
-    create_observation,
-    create_sampling_rc,
-    draw_sampling_opts,
-    get_images,
-    simulate_data_set,
-    test_opts,
-)
+from .data_set import SimulateDataSet
 from .observation import Baselines, Observation, ValidBaselineSubset
 from .scan import angularDistance, calc_beam, calc_fourier, integrate, jinc, rime
 from .visibility import Visibilities, generate_noise, vis_loop
@@ -17,22 +8,16 @@ __all__ = [
     "Array",
     "Baselines",
     "Observation",
+    "SimulateDataSet",
     "ValidBaselineSubset",
     "Visibilities",
     "angularDistance",
     "calc_beam",
     "calc_fourier",
-    "calc_ref_elev",
     "calc_time_steps",
-    "create_observation",
-    "create_sampling_rc",
-    "draw_sampling_opts",
     "generate_noise",
-    "get_images",
     "integrate",
     "jinc",
     "rime",
-    "simulate_data_set",
-    "test_opts",
     "vis_loop",
 ]
