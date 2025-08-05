2f1bf3b6f28328d69e6b6e84a7e4133342ce0e94
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-08-05 10:55:47 +0200

    Add docstring to setup_logger

--- src/pyvisgen/utils/logging.py
+++ src/pyvisgen/utils/logging.py
@@ -4,6 +4,26 @@ from rich.logging import RichHandler
 
 
 def setup_logger(**kwargs):
+    """Basic logging setup. Uses :class:`~rich.logging.RichHandler`
+    for formatting and highlighting of the log.
+
+    Parameters
+    ----------
+    **kwargs
+        Keyword arguments for :class:`~rich.logging.RichHandler`.
+
+    Returns
+    -------
+    logging.Logger
+        Logger object using :class:`~rich.logging.RichHandler`
+        for formatting and highlighting.
+
+    See Also
+    --------
+    :class:`~rich.logging.RichHandler` :
+        Rich's builtin logging handler for more information on
+        allowed keyword arguments.
+    """
     FORMAT = "%(message)s"
 
     logging.basicConfig(
