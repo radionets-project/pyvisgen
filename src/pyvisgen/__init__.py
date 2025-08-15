03a9827143f44488933fb44867dfc3f4c253344c
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-08-15 08:54:33 +0200

    Use rich traceback

--- src/pyvisgen/__init__.py
+++ src/pyvisgen/__init__.py
@@ -4,6 +4,11 @@ pyvisgen - Python implementation of the VISGEN tool.
 Licensed under a MIT style license - see LICENSE
 """
 
+from rich.traceback import install
+
 from .version import __version__
 
 __all__ = ["__version__"]
+
+
+install(show_locals=True)
