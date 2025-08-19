32900da616a24183bc353dcb52d9c09b0545b317
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-08-19 13:36:58 +0200

    Fix key in test_read_conf(), remove unused test functions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -39,39 +39,8 @@ def test_read_config():
         "in_path",
         "out_path_fits",
         "out_path_gridded",
-        "file_prefix",
+        "dataset_type",
     ]
-
-
-# def test_single_occurance():
-#    from pyvisgen.simulation.utils import single_occurance
-#
-#    arr = np.array([1, 2, 2, 3, 4, 4])
-#
-#    indx = single_occurance(arr)
-#
-#    assert (indx == np.array([0, 1, 3, 4])).all()
-
-
-# def test_get_pairs():
-#    from pyvisgen.layouts.layouts import get_array_layout
-#    from pyvisgen.simulation.utils import get_pairs
-#
-#    layout = get_array_layout("eht")
-#    delta_x, delta_y, delta_z = get_pairs(layout)
-#
-#    assert delta_x.shape == (56, 1)
-#    assert delta_x.shape == delta_y.shape == delta_z.shape
-
-
-# def test_calc_time_steps():
-#    from pyvisgen.utils.config import read_data_set_conf
-#    from pyvisgen.simulation.utils import calc_time_steps
-#
-#    conf = read_config("config/default_data_set.toml")
-#    time = calc_time_steps(conf)
-#
-#    assert time.shape == (2232,)
 
 
 def test_Array():
