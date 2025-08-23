dd21a677e82b2773abd3bba74c26dd684d10f2c3
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-08-23 14:19:53 +0200

    Update plugin manager

---
+++ src/pyvisgen/_plugin_manager.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+from importlib.metadata import entry_points
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+from pyvisgen import __version__
+from pyvisgen.utils.logging import setup_logger
+
+LOGGER = setup_logger(__name__)
+
+
+class PluginManager:
+    """Plugin manager class for pyvisgen."""
+
+    def _gridding(self) -> dict[str, Callable]:
+        """Classmethod that discovers all available gridding plugins.
+
+        Returns
+        -------
+        dict
+            Dictionary containing all available gridding plugins
+            in 'pyvisgen.gridding' entry point group.
+        """
+        self.plugins = {}
+
+        eps = entry_points()
+        if hasattr(eps, "select"):
+            gridding_plugins = eps.select(group="pyvisgen.gridding")
+
+        for entry_point in gridding_plugins:
+            try:
+                plugin_class = entry_point.load()
+                self.plugins[entry_point.name] = plugin_class
+            except ImportError as e:
+                LOGGER.warn(f"Failed to load plugin {entry_point.name}: {e}")
+
+        return self.plugins
+
+    @classmethod
+    def get_gridder(cls, name: str = "pyvisgrid.gridder") -> Callable:
+        """Get a specific gridding plugin by name.
+
+        Parameters
+        ----------
+        name : str, optional
+            Name of the gridder plugin. The plugin has to be part of
+            the 'pyvisgen.gridding' entry point group. Raises a ValueError
+            if no plugin is found in that group. Default: ``'pyvisgrid.gridder'``
+
+        Return
+        ------
+        instance or callable
+            Gridder plugin for the given name.
+
+        Raises
+        ------
+        ValueError
+            If no plugins are found in the 'pyvisgen.gridding' entry point group.
+        ValueError
+            If plugin 'name' was not found.
+        """
+        instance = cls()
+
+        plugins = instance._gridding()
+        if not list(plugins.keys()):
+            raise ValueError(
+                "No plugins available in entry point group 'pyvisgen.gridding'! "
+                "Make sure you have installed a package providing plugins compatible "
+                f"with pyvisgen {__version__}, e.g. pyvisgrid.gridder from pyvisgrid "
+                "(uv pip install pyvisgrid)!"
+            )
+        if name not in plugins:
+            raise ValueError(
+                f"Plugin '{name}' not found. Available: {list(plugins.keys())}"
+            )
+
+        return plugins[name]
