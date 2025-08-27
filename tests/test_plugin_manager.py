1f1c0775a6347dbf8e5166ff481e04f016ffcc1f
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-08-27 10:48:52 +0200

    Add test for gridder fallback in SimulateDataset

--- tests/test_plugin_manager.py
+++ tests/test_plugin_manager.py
@@ -1,6 +1,9 @@
+import logging
+
 from numpy.testing import assert_raises
 
 from pyvisgen._plugin_manager import Manager, PluginManager
+from pyvisgen.utils.config import read_data_set_conf
 
 
 def test_get_avail_plugins():
@@ -17,5 +20,21 @@ def test_get_gridder():
     assert_raises(
         ValueError,
         manager.get_gridder,
-        "this_plugin_does_not_exist"
+        "this_gridder_plugin_does_not_exist"
     )
+
+def test_gridder_fallback(caplog):
+    """Tests fallback if gridder plugin is not available
+    when called in pyvisgen.dataset.SimulateDataset
+    """
+    from pyvisgen.dataset import SimulateDataSet
+
+    s = SimulateDataSet
+
+    config = read_data_set_conf("tests/test_conf.toml")
+    config["gridder"] = "this_gridder_plugin_does_not_exist"
+
+    with caplog.at_level(logging.WARN):
+        s.from_config(config)
+
+    assert "Falling back to default gridder!" in caplog.text
