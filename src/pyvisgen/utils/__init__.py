fb643bc26bc34f8f35e0fffb73864cb8314e84bb
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-10-28 14:17:05 +0100

    Delete old config system

--- src/pyvisgen/utils/__init__.py
+++ src/pyvisgen/utils/__init__.py
@@ -1,10 +1,8 @@
-from .config import read_data_set_conf
 from .data import load_bundles, open_bundles
 from .logging import setup_logger
 
 __all__ = [
     "load_bundles",
     "open_bundles",
-    "read_data_set_conf",
     "setup_logger",
 ]
