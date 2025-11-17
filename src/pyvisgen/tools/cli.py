09d0cda92c2a3eb95db822594c92e6cc50b51828
Anno Knierim <anno.knierim@tu-dortmund.de>
2025-11-17 09:39:39 +0100

    Add new main CLI file

---
+++ src/pyvisgen/tools/cli.py
@@ -0,0 +1,36 @@
+import rich_click as click
+
+from pyvisgen import __version__
+
+from .create_dataset import main as create_dataset
+from .quickstart import main as quickstart
+
+click.rich_click.COMMAND_GROUPS = {
+    "pyvisgen": [
+        {
+            "name": "Simulation",
+            "commands": ["simulate"],
+        },
+        {
+            "name": "Setup",
+            "commands": ["quickstart"],
+        },
+    ]
+}
+
+
+@click.group(
+    help=f"""
+    This is the [dark_orange]pyvisgen[/]
+    [cornflower_blue]v{__version__}[/] main CLI tool.
+    """
+)
+def main():
+    pass
+
+
+main.add_command(quickstart, name="quickstart")
+main.add_command(create_dataset, name="simulate")
+
+if __name__ == "__main__":
+    main()
