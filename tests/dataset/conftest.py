f1da6d6c4771f3d57251ae840fe7d7416c3bdf8f
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-01-07 19:34:30 +0100

    Add conftest.py for dataset module

---
+++ tests/dataset/conftest.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+
+from pyvisgen.dataset.dataset import DATEFMT, SimulateDataSet
+from pyvisgen.io import Config
+
+
+@pytest.fixture
+def simulate_dataset() -> SimulateDataSet:
+    return SimulateDataSet()
+
+
+@pytest.fixture
+def _simulate_dataset() -> type[SimulateDataSet]:
+    return SimulateDataSet
+
+
+@pytest.fixture
+def sd_sampling(simulate_dataset: SimulateDataSet) -> SimulateDataSet:
+    simulate_dataset.conf = Config.from_toml("tests/test_conf.toml")
+    simulate_dataset.rng = np.random.default_rng()
+    simulate_dataset.date_fmt = DATEFMT
+    simulate_dataset.device = "cpu"
+    simulate_dataset.multiprocess = -1
+    simulate_dataset.overall_task_id = 1
+
+    return simulate_dataset
