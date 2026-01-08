92b605bb56d91000658535291ffb697c76d5ef33
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-01-08 19:14:52 +0100

    Use pytest-mock mocker to mock progress bars

--- tests/dataset/conftest.py
+++ tests/dataset/conftest.py
@@ -11,12 +11,16 @@ def simulate_dataset() -> SimulateDataSet:
 
 
 @pytest.fixture
-def sd_sampling(simulate_dataset: SimulateDataSet) -> SimulateDataSet:
+def sd_sampling(mocker, simulate_dataset: SimulateDataSet) -> SimulateDataSet:
     simulate_dataset.conf = Config.from_toml("tests/test_conf.toml")
     simulate_dataset.rng = np.random.default_rng()
     simulate_dataset.date_fmt = DATEFMT
     simulate_dataset.device = "cpu"
     simulate_dataset.multiprocess = -1
     simulate_dataset.overall_task_id = 1
+
+    mocker.patch("pyvisgen.dataset.dataset.bundles_progress")
+    mocker.patch("pyvisgen.dataset.dataset.overall_progress")
+    mocker.patch("pyvisgen.dataset.dataset.current_bundle_progress")
 
     return simulate_dataset
