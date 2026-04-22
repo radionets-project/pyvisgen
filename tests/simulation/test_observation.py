b65a98e42c4458dde26b2945937031673cc18092
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-04-22 16:21:58 +0200

    fix(tests): compare all elements in fields assertion

--- tests/simulation/test_observation.py
+++ tests/simulation/test_observation.py
@@ -79,7 +79,7 @@ class TestValidBaselineSubset:
         assert isinstance(subset_item, ValidBaselineSubset)
 
         for f in fields(ValidBaselineSubset):
-            assert getattr(subset_item, f.name) == getattr(subset, f.name)[idx]
+            assert (getattr(subset_item, f.name) == getattr(subset, f.name)[idx]).all()
 
     def test_get_slice(self, subset: ValidBaselineSubset) -> None:
         subset_items = subset[2:-2]
