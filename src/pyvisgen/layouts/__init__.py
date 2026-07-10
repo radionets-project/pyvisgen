42d13ea8ca8b599fcce67f76447a0ebc4db8b89c
Anno Knierim <anno.knierim@tu-dortmund.de>
2026-07-10 11:50:05 +0200

    feat: add peek method to Stations, add st_names attr

--- src/pyvisgen/layouts/__init__.py
+++ src/pyvisgen/layouts/__init__.py
@@ -1,6 +1,8 @@
+from .disp import ArrayDisplay
 from .layouts import Stations, get_array_layout, get_array_names
 
 __all__ = [
+    "ArrayDisplay",
     "Stations",
     "get_array_layout",
     "get_array_names",
